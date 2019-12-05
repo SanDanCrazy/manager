4ed1ffb8d6e0dad989e35fd4ddf80c02f2cd3fb5
wangpeng <wangpeng@fscut.com>
2019-12-05 18:12:53 +0800

    1.创建storyCtrl demo proj

---
+++ storyctrl/src/test/java/com/softctrl/storyctrl/StoryctrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softctrl.storyctrl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryctrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
