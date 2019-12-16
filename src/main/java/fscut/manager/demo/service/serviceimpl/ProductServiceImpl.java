99076749751acaf804050a7614712c198db92969
daKang <1017243931@qq.com>
2019-12-16 12:20:26 +0800

    12/16 merge

--- src/main/java/fscut/manager/demo/service/serviceimpl/ProductServiceImpl.java
+++ src/main/java/fscut/manager/demo/service/serviceimpl/ProductServiceImpl.java
@@ -1,4 +1,4 @@
-package fscut.manager.demo.service.serviceImpl;
+package fscut.manager.demo.service.serviceimpl;
 
 import fscut.manager.demo.dao.ProductRepository;
 import fscut.manager.demo.entity.Product;
