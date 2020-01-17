28530f23cb9b4de802429ca3f805c11961198776
daKang <1017243931@qq.com>
2020-01-17 13:44:35 +0800

    1/17 make swagger

---
+++ src/test/java/fscut/manager/demo/Documentation.java
@@ -0,0 +1,115 @@
+package fscut.manager.demo;
+
+import fscut.manager.demo.config.SpringfoxSwagger2Config;
+import io.github.swagger2markup.GroupBy;
+import io.github.swagger2markup.Language;
+import io.github.swagger2markup.Swagger2MarkupConfig;
+import io.github.swagger2markup.Swagger2MarkupConverter;
+import io.github.swagger2markup.builder.Swagger2MarkupConfigBuilder;
+import io.github.swagger2markup.markup.builder.MarkupLanguage;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import springfox.documentation.staticdocs.SwaggerResultHandler;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@AutoConfigureMockMvc
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = {DemoApplication.class , SpringfoxSwagger2Config.class})
+@WebAppConfiguration
+public class Documentation {
+
+    private String outputDir = "target/asciidoc";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void generateAsciiDocs() throws Exception {
+        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+                .withMarkupLanguage(MarkupLanguage.ASCIIDOC)
+                .withOutputLanguage(Language.ZH)
+                .withPathsGroupedBy(GroupBy.TAGS)
+                .withGeneratedExamples()
+                .withoutInlineSchema()
+                .build();
+
+        Swagger2MarkupConverter.from(new URL("http://localhost:8080/v2/api-docs"))
+                .withConfig(config)
+                .build()
+                .toFolder(Paths.get("./docs/asciidoc/generated"));
+    }
+
+    @Test
+    public void generateAsciiDocsToFile() throws Exception {
+        //    输出Ascii到单文件
+        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+                .withMarkupLanguage(MarkupLanguage.ASCIIDOC)
+                .withOutputLanguage(Language.ZH)
+                .withPathsGroupedBy(GroupBy.TAGS)
+                .withGeneratedExamples()
+                .withoutInlineSchema()
+                .build();
+
+        Swagger2MarkupConverter.from(new URL("http://localhost:8080/v2/api-docs"))
+                .withConfig(config)
+                .build()
+                .toFile(Paths.get("./docs/asciidoc/generated/all"));
+    }
+
+    @Test
+    public void generateConfluenceDocs() throws Exception {
+        //    输出Confluence使用的格式
+        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+                .withMarkupLanguage(MarkupLanguage.CONFLUENCE_MARKUP)
+                .withOutputLanguage(Language.ZH)
+                .withPathsGroupedBy(GroupBy.TAGS)
+                .withGeneratedExamples()
+                .withoutInlineSchema()
+                .build();
+
+        Swagger2MarkupConverter.from(new URL("http://localhost:8080/v2/api-docs"))
+                .withConfig(config)
+                .build()
+                .toFolder(Paths.get("./docs/confluence/generated"));
+    }
+
+    @Test
+    public void generateMarkdownDocsToFile() throws Exception {
+        //    输出Markdown到单文件
+        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+                .withMarkupLanguage(MarkupLanguage.MARKDOWN)
+                .withOutputLanguage(Language.ZH)
+                .withPathsGroupedBy(GroupBy.TAGS)
+                .withGeneratedExamples()
+                .withoutInlineSchema()
+                .build();
+
+        Swagger2MarkupConverter.from(new URL("http://localhost:8080/v2/api-docs"))
+                .withConfig(config)
+                .build()
+                .toFile(Paths.get("./docs/markdown/generated/all"));
+    }
+}
